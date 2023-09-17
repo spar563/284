f250e7675b39746a3e1db7b5290ba305951e1f40
Seoha <spar563@aucklanduni.ac.nz>
2023-09-18 02:14:32 +1200

    Assignment 2: Update Pattern

--- A2/Pattern.java
+++ A2/Pattern.java
@@ -4,44 +4,46 @@ import java.io.InputStreamReader;
 
 public class Pattern {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String pattern = br.readLine();
-        String text = br.readLine();
-        int occurrences = 0;
+    String pattern = br.readLine();
+    String text = br.readLine();
+    int occurrences = 0;
 
-        // Calculate the hash code of the pattern
-        int patternHash = StringHash(pattern);
+    // Calculate the hash code of the pattern
+    int patternHash = StringHash(pattern);
 
-        // Calculate the initial hash code of the first substring in the text
-        int textHash = StringHash(text.substring(0, pattern.length()));
-
-        for (int i = 0; i <= text.length() - pattern.length(); i++) {
-            if (textHash == patternHash) {
-                boolean isMatch = true;
-                for (int j = 0; j < pattern.length(); j++) {
-                    if (text.charAt(i + j) != pattern.charAt(j)) {
-                        isMatch = false;
-                        break;
-                    }
-                }
-                if (isMatch) {
-                    occurrences++;
-                }
-            }
+    for (int i = 0; i <= text.length() - pattern.length() + 1; i++) {
+      // rolling hash for the next window for text
+      if (i < text.length() - pattern.length()) {
+        // Add the next character
+        textHash = StringHash(text.substring(i, pattern.length() + i));
+      }
+      if (textHash == patternHash) {
+        boolean isMatch = true;
+        for (int j = 0; j < pattern.length(); j++) {
+          if (text.charAt(i + j) != pattern.charAt(j)) {
+            isMatch = false;
+            break;
+          }
         }
-        System.out.println(occurrences);
-    }
-
-    // Function to calculate the hash code of a string
-    public static int StringHash(String str) {
-        int hash = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            hash = (31 * hash + str.charAt(i));
+        if (isMatch) {
+          occurrences++;
         }
-
-        return hash;
+      }
     }
+    System.out.println(occurrences);
+  }
+
+  // Function to calculate the hash code of a string
+  public static int StringHash(String str) {
+    int hash = 0;
+    int power = 31;
+
+    for (int i = 0; i < str.length(); i++) {
+      hash = (power * hash + str.charAt(i));
+    }
+    return hash;
+  }
 }
