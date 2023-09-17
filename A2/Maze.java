0bdf6ccd55d2ab2aff229a8d07437aba7f2e339b
Seoha <spar563@aucklanduni.ac.nz>
2023-09-17 16:32:29 +1200

    Assignment 2: Update Maze.java
    initialize table
    print table

--- A2/Maze.java
+++ A2/Maze.java
@@ -12,17 +12,49 @@ public class Maze {
     int n = in.nextInt();
     int m = in.nextInt();
 
-    // YOUR CODE GOES HERE.
-    /* 
-    1. Create a data structure to represent the table/grid.
-    2. Initialize the grid with walls (e.g., using a 2D array).
-    3. Iterate through the pairs of adjacent cells, and for each pair, check if there is a path between them (you can use algorithms like Depth-First Search or Union-Find to check connectivity).
-    4. If there is no path between the cells, destroy the wall between them.
-    5. Output the resulting table/grid. 
-    */
+    // YOUR CODE GOES HERE. 
+
+    // Initialize disjoint-set data structure
+    int table_size = n * n;
+    int[] parent = new int[table_size];  // Parent array for disjoint-set
+    int[] rank = new int[table_size];    // Rank array for disjoint-set
+    char[][] table = initializetable(n);
+
+    // Print the resulting maze
+    printMaze(out, table, n);
+    
 
     out.close();
   }
+
+  static char[][] initializetable(int n) {
+    int x = n * 2 + 1;
+    
+    char[][] table = new char[x][x];
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < x; j++) {
+            if (i % 2 == 0 && j % 2 == 0) {
+                table[i][j] = '+';
+            } else if (i % 2 == 0) {
+                table[i][j] = '-';
+            } else if (j % 2 == 0) {
+                table[i][j] = '|';
+            } else {
+                table[i][j] = ' ';
+            }
+        }
+    }
+    return table;
+}
+
+static void printMaze(PrintWriter out, char[][] table, int n) {
+  for (int i = 0; i < n * 2 + 1; i++) {
+      for (int j = 0; j < n * 2 + 1; j++) {
+          out.print(table[i][j]);
+      }
+      out.println();
+  }
+}
 
   static class FastScanner {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
