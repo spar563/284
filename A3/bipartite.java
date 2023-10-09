553fc505bca977567eaabf5a2d5df08f33515d08
Seoha <spar563@aucklanduni.ac.nz>
2023-10-09 13:36:16 +1300

    Update bipartite.java

--- A3/bipartite.java
+++ A3/bipartite.java
@@ -4,43 +4,53 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Bipartite {
   private int order;
   private List<List<Integer>> adjList;
   private boolean isBipartite;
 
-  private boolean isBipartite() {
+private boolean isBipartite() {
     int[] colors = new int[order];
     Arrays.fill(colors, -1); // -1 represents no color (unvisited)
 
     for (int i = 0; i < order; i++) {
-      if (colors[i] == -1) {
-        if (!bipartiteDFS(i, colors, 0)) {
-          return false;
+        if (colors[i] == -1) {
+            if (!bipartiteBFS(i, colors)) {
+                return false;
+            }
         }
-      }
     }
 
     return true;
-  }
+}
 
-  private boolean bipartiteDFS(int node, int[] colors, int color) {
-    colors[node] = color;
+private boolean bipartiteBFS(int start, int[] colors) {
+    Queue<Integer> queue = new LinkedList<>();
+    queue.offer(start);
+    colors[start] = 0; // Mark the starting node as color 0
 
-    for (int neighbor : adjList.get(node)) {
-      if (colors[neighbor] == color) {
-        return false; // Not bipartite if neighboring nodes have the same color
-      }
+    while (!queue.isEmpty()) {
+        int current = queue.poll();
+        int currentColor = colors[current];
 
-      if (colors[neighbor] == -1 && !bipartiteDFS(neighbor, colors, 1 - color)) {
-        return false; // Recursively visit neighbors with the opposite color
-      }
+        for (int neighbor = 0; neighbor < order; neighbor++) {
+            if (adjList.get(current).contains(neighbor)) {
+                if (colors[neighbor] == -1) {
+                    colors[neighbor] = 1 - currentColor; // Assign the opposite color
+                    queue.offer(neighbor);
+                } else if (colors[neighbor] == currentColor) {
+                    return false;
+                }
+            }
+        }
     }
 
     return true;
-  }
+}
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -55,12 +65,11 @@ public class Bipartite {
       Bipartite bipartiteDetector = new Bipartite();
       bipartiteDetector.order = order;
       bipartiteDetector.adjList = new ArrayList<>(order);
-      for (int i = 0; i < order; i++) {
-        bipartiteDetector.adjList.add(new ArrayList<>());
-      }
+
 
       // Read the graph's adjacency lists
       for (int i = 0; i < order; i++) {
+        bipartiteDetector.adjList.add(new ArrayList<>());
         String[] input = br.readLine().split(" ");
         for (String string : input) {
           if (!string.isEmpty()) {
